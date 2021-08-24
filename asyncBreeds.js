6debd04a71c4e7d96e251a7b343193049c851eba
Jose Bautista <Cooldj1020@gmail.com>
2021-08-24 13:30:21 -0600

    reading and evaluating files from disk asynchronously

---
+++ asyncBreeds.js
@@ -0,0 +1,22 @@
+// asyncBreeds.js
+const fs = require('fs');
+
+const breedDetailsFromFile = function(breed, callback) {
+  console.log('breedDetailsFromFile: Calling readFile...');
+  fs.readFile(`./data/${breed}.txt`, 'utf8', (error, data) => {
+    // CHANGE: Pass data into callback instead of returning it directly
+    console.log("In readFile's Callback: it has the data.");
+    if (error) {
+      return undefined;
+    }
+    callback(data);
+  });
+};
+
+// CHANGE 1: Moved the console.log into a new function:
+const printOutCatBreed = (breed) => {
+  console.log('Return Value: ', breed); // => print out details correctly.
+};
+// console.log(breedDetailsFromFile('balinese', printOutCatBreed));
+
+module.exports = breedDetailsFromFile;
